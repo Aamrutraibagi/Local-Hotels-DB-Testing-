3b1b4b0719a7be0f6ce443dd3c3c62fd4e3fb8b4
Atri Biswas <atribiswas@Atris-MacBook-Air.local>
2023-11-11 21:18:06 +0530

    windows alt added

---
+++ setup.bat
@@ -0,0 +1,28 @@
+@echo off
+
+REM MySQL credentials
+set MYSQL_USER=root
+set MYSQL_PASS=
+
+REM Database name
+set DB_NAME=hotels
+
+REM SQL file path
+set SQL_FILE=db_data\hotels.sql
+
+REM Drop the existing database if it exists and create a new one
+echo Dropping and recreating the database...
+mysql -u%MYSQL_USER% -p%MYSQL_PASS% -e "DROP DATABASE IF EXISTS %DB_NAME%; CREATE DATABASE %DB_NAME%;"
+
+REM Check if the SQL file exists
+if exist "%SQL_FILE%" (
+    REM Import the SQL file into the new database
+    echo Importing data from %SQL_FILE%...
+    mysql -u%MYSQL_USER% -p%MYSQL_PASS% %DB_NAME% < "%SQL_FILE%"
+    echo Data import complete.
+) else (
+    echo Error: SQL file %SQL_FILE% not found.
+    exit /b 1
+)
+
+echo Database refresh complete.
